3852be09f1f0d39b8443ec422a5d729ba31b039a
Ghdhdhhake <114733000+Ghdhdhhake@users.noreply.github.com>
2026-08-13 11:59:22 +0800

    demo-1
    test

---
+++ 2-5/2-5-1.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+#define NAME "Javi"
+#define SURNAME "Wang"
+
+int main(void)
+{
+    printf("%s %s\n", NAME, SURNAME);
+    return 0;
+}
---
+++ 3-7/3-7-2.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+
+int main(void)
+{
+    int input;
+    printf("Enter a value of char int ASCII:");
+    scanf("%d", &input);
+    printf("Your input value is %d, and char is %c. \n", input, input);
+    return 0;
+}
